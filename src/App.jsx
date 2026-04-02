ce4efcaa8126e7b60e7d422a5ee0ee68c40e86bb
Annushka-chauhan <anushkachauhanannu@gmail.com>
2026-04-02 13:37:21 +0530

    Updated File

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -6,6 +6,7 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
+import { use } from 'react';
 const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 
 const storedPlaces = storedIds.map(id =>
@@ -38,7 +39,7 @@ useEffect(() =>{
   
 
 
-
+  
   function handleStartRemovePlace(id) {
     setModalIsOpen(true)
     selectedPlace.current = id;
@@ -47,7 +48,6 @@ useEffect(() =>{
   function handleStopRemovePlace() {
    setModalIsOpen(false)
   }
-
   function handleSelectPlace(id) {
     setPickedPlaces((prevPickedPlaces) => {
       if (prevPickedPlaces.some((place) => place.id === id)) {
@@ -62,15 +62,20 @@ useEffect(() =>{
     );
    }
   }
-
-  function handleRemovePlace() {
+  //this returns a function but this is not re executed again when this is called again and again just like we are entering to the
+  //infinite loop when the onConfirm is sent as an argument 
+  //by useCallback its that the function is not recreated but its stored internally into the memory and reuses the stored function 
+  //whenever the component function is executed again 
+   const handleRemovePlace = useCallback( function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     setModalIsOpen(false)
     const storedIds = JSON.parse( localStorage.getItem('selectedPlaces')) || [];
-    localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current )))
-  }
+    localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current ))
+  );
+  },[]);
+ 
 
   return (
     <>
